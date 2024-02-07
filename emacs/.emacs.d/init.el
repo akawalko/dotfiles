e2d06c4c0665e9001d443d7d8fb6085957d4f668
Arkadiusz Kawałko <arek.kawalko@gmail.com>
2024-02-07 18:27:25 +0100

    Heavily changed emacs config in effort to build my custom IDE out of it.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -10,7 +10,7 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :height 180)
+(set-face-attribute 'default nil :height 170)
 
 (load-theme 'wombat)
 
@@ -97,24 +97,63 @@
   (ivy-mode 1))
 
 (use-package counsel)
-
 ;; (keymap-global-set "C-x b" 'counsel-switch-buffer)
+
+(use-package counsel-etags)
 
 (use-package beacon
   :init
   (beacon-mode 1))
 
 (use-package company)
-
 (add-hook 'after-init-hook 'global-company-mode)
+
+(use-package company-statistics)
+(add-hook 'after-init-hook 'company-statistics-mode)
 
 (use-package avy)
 
 (global-set-key (kbd "C-:") 'avy-goto-char)
 
+(use-package ace-window
+  :bind ("M-p" . ace-window)
+  :delight
+  :config (ace-window-display-mode 1))
+
+;; maybe try window-numbering
+
 (use-package which-key)
 
 (which-key-mode)
+
+(use-package projectile)
+
+(use-package dashboard
+  :preface
+  (defun ajk/dashboard-banner ()
+    "Set a dashboard banner including information on package initialization
+  time and garbage collections."
+    (setq dashboard-banner-logo-title
+          (format "Emacs ready in %.2f seconds with %d garbage collections."
+                  (float-time (time-subtract after-init-time before-init-time)) gcs-done)))
+  :config
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5)))
+  ;;(setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
+  (setq dashboard-banner-logo-title nil)
+  ;;(setq dashboard-init-info "I use Emacs, which might be thought of as a thermonuclear word processor.' - Neal Stephenson")
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-center-content t)
+  :hook ((after-init     . dashboard-refresh-buffer)
+         ;;(dashboard-mode . ajk/dashboard-banner)
+	 )
+  
+  (dashboard-setup-startup-hook)
+)
 
 (use-package amx)
 
@@ -122,3 +161,161 @@
   :ensure nil)
 
 (use-package php-mode)
+
+;;company-php
+(setq programming-packages '(lsp-mode yasnippet lsp-treemacs lsp-ui lsp-ivy flycheck company-php ac-php ac-php-core dap-mode phpactor))
+
+(when (cl-find-if-not #'package-installed-p programming-packages)
+  (package-refresh-contents)
+  (mapc #'package-install programming-packages))
+
+(add-hook 'php-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+;;  :hook (lsp-mode . efs/lsp-mode-setup)
+  :config
+  (setq lsp-clients-php-server-command "/home/arkadiusz/.emacs.d/elpa/phpactor-20221023.608/")
+  (setq lsp-phpactor-path "/home/arkadiusz/.emacs.d/elpa/phpactor-20221023.608/phpactor")
+  (setq lsp-enabled-clients '(phpactor))
+  (setq lsp-disabled-clients '(php-ls iph intelephense serenata))
+  (lsp-enable-which-key-integration t)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+)
+
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-php)
+  (yas-global-mode))
+
+  ;; (add-hook 'php-mode-hook
+  ;;           '(lambda ()
+  ;;              (auto-complete-mode t)
+  ;;              (require 'ac-php)
+  ;;              (setq ac-sources '(ac-source-php))
+  ;;              (yas-global-mode 1)
+
+  ;;              (define-key php-mode-map (kbd "C-]")
+  ;;                'ac-php-find-symbol-at-point)
+  ;;              (define-key php-mode-map (kbd "C-t")
+  ;;                'ac-php-location-stack-back)))
+
+
+(use-package lsp-ui
+  :ensure t
+  :requires flycheck
+  :after lsp-mode
+  :config
+  (setq
+   lsp-ui-doc-enable t
+   lsp-ui-doc-use-childframe t
+   lsp-ui-doc-position 'top
+   lsp-ui-doc-include-signature t
+
+   lsp-ui-flycheck-enable t
+   lsp-ui-flycheck-list-position 'right
+   lsp-ui-flycheck-live-reporting t
+
+   lsp-ui-imenu-enable t
+
+   lsp-ui-peek-enable t
+   lsp-ui-peek-list-width 60
+   lsp-ui-peek-peek-height 25
+
+   lsp-ui-sideline-enable t
+   lsp-ui-sideline-update-mode 'line
+   lsp-ui-sideline-show-code-actions t
+   lsp-ui-sideline-show-hover nil)
+
+  :bind
+  (:map lsp-ui-mode-map
+	("C-c C-j" . lsp-ui-peek-find-definitions)
+	("C-c i"   . lsp-ui-peek-find-implementation)
+	("C-c m"   . lsp-ui-imenu))
+
+  :hook
+  (lsp-mode . lsp-ui-mode))
+
+
+(setq phpactor-executable "/home/arkadiusz/.emacs.d/elpa/phpactor-20221023.608/phpactor")
+
+;; packages to try:
+;; - paradox,
+;; - mmm-mode
+;; - restclient,
+;; - js2-mode,
+;; - expand-region (works like C-w in PhpStorm)
+;; - general
+;; - helpful
+;; - dashboard
+;; - web-mode
+;; - yaml-mode
+;; - indent-guide
+;; - visual-regexp
+;; - visual-regexp-steroids.el
+;; - evil-leade
+;; - zencoding-mode
+;; - iedit
+;; - deadgrep
+;; - switch-window
+;; - persp-mode.el or perspective-el
+;; - company-quickhelp
+;; - emojify
+;; - easy-kill
+;; - counsel-projectil
+;; - multi-web-mode
+;; - bm
+;; - ws-butler
+;; - auto-yasnippet
+;; - modalka
+;; - rg
+;; - company-prescient
+;; - ivy-prescient
+;; - prescient
+;; - string-inflection
+;; - symbol-overlay
+;; - mark-multiple
+;; - yafolding
+;; - wrap-region
+;; - elmacro
+;; - composable
+;; - diminish
+;; - simpleclip
+;; - ethan-wspace
+;; - phi-search
+;; - swoop
+;; - whitespace-clean
+;; - ace-isearch
+;; - persp-projectile
+;; - corral
+;; - frames-only-mode
+;; - move-text
+
+;; something disabled this, so I reintroduce this symbol/variable
+(setq xref-auto-jump-to-first-xref t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ace-window-posframe-mode t nil (ace-window))
+ '(ispell-dictionary nil)
+ '(package-selected-packages
+   '(counsel-etags dashboard emacs-dashboard paradox company-statistics projectile company-php phpactor php-mode ac-php lsp-treemacs lsp-mode yasnippet which-key use-package magit lsp-ui lsp-ivy flycheck dap-mode counsel company beacon amx ac-php-core))
+ '(paradox-github-token t)
+ '(projectile-mode t nil (projectile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
